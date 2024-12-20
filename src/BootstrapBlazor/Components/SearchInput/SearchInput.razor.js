f9f84c1b352b8266f61a1b1cbe642c6231d51de2
linpanxing <wapco@21cn.com>
2024-12-20 16:57:53 +0800

    搜索输入组件优化

--- src/BootstrapBlazor/Components/SearchInput/SearchInput.razor.js
+++ src/BootstrapBlazor/Components/SearchInput/SearchInput.razor.js
@@ -1,7 +1,32 @@
-﻿let timeoutId;
-export function onValueChanged(component, value) {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(async () => {
-        await component.invokeMethodAsync('SearchValueChanged', value);
-    }, 500);
+﻿import {debounce} from "../../modules/utility.js"
+import EventHandler from "../../modules/event-handler.js"
+
+export function init(component, elem, changeOnEnter) {
+    const onSearchValueChange = debounce(v => component.invokeMethodAsync("SearchValueChanged", v));
+    let isComposing = false;
+
+    EventHandler.on(elem, 'input', '.search-text', e => {
+        if (isComposing) {
+            return;
+        }
+        if (changeOnEnter && e.key !== 'Enter') {
+            return;
+        }
+        onSearchValueChange(e.delegateTarget.value);
+    });
+
+    EventHandler.on(elem, 'compositionstart', '.search-text', e => {
+        isComposing = true;
+    });
+
+    EventHandler.on(elem, 'compositionend', '.search-text', e => {
+        isComposing = false;
+        onSearchValueChange(e.delegateTarget.value);
+    });
+
+    EventHandler.on(elem, 'click', '.search-clear', e => {
+        component.invokeMethodAsync("SearchValueChanged", "");
+        e.preventDefault();
+        e.stopPropagation();
+    })
 }
